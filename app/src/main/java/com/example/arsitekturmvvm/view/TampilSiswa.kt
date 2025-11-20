9fabcd4d2247cef2141caae9a8d745c87a180f6c
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 16:16:57 +0700

    feat: deklarasi function TampilSiswa dengan parameter 'statusUiSiswa' untuk mengirimkan data input dan button

---
+++ app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
@@ -0,0 +1,13 @@
+package com.example.arsitekturmvvm.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: siswa,
+    onBackButtonClicked:() ->Unit,
+){
+
+}
