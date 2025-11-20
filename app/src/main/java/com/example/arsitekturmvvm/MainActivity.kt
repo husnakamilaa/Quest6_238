23a75bd3a9da20403dd70738f3c597f9a2b7a971
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 18:01:54 +0700

    feat: deklarasi function Navigasi untuk menjalankan app di main activity

--- app/src/main/java/com/example/arsitekturmvvm/MainActivity.kt
+++ app/src/main/java/com/example/arsitekturmvvm/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.arsitekturmvvm.ui.theme.ArsitekturMVVMTheme
+import com.example.arsitekturmvvm.view.Navigasi.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArsitekturMVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    SiswaApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ArsitekturMVVMTheme {
-        Greeting("Android")
-    }
-}
