605f6b617932fb673587e4969851d68eb9bed43a
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 16:51:24 +0700

    feat: menambahkan space untuk jarak vertikal dengan button

--- app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
@@ -2,6 +2,8 @@ package com.example.arsitekturmvvm.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -55,6 +57,7 @@ fun TampilSiswa(
                     }
                     Divider(thickness = dimensionResource(R.dimen.thickness_divider))
                 }
+                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
             }
         }
     }
