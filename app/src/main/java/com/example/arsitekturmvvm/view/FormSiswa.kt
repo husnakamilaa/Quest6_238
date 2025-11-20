8b6a9fffaa7352e6c1b0dd522e5350182da5c1f6
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 11:56:32 +0700

    feat: menambahkan parameter pilihanJK sebagai databinding, onsubmitbutton untuk navigasi button submit, dan parameter modifier

---
+++ app/src/main/java/com/example/arsitekturmvvm/view/FormSiswa.kt
@@ -0,0 +1,27 @@
+package com.example.arsitekturmvvm.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.arsitekturmvvm.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>)-> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
