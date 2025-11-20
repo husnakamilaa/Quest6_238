c5e7c750ee6aa93bc4e2a5357d433935cea115b1
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 15:51:23 +0700

    feat: deklarasi button dengan mengaktifkan button apabila alamat tidak kosong

--- app/src/main/java/com/example/arsitekturmvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/FormSiswa.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -113,6 +115,12 @@ fun FormSiswa(
                 }
             )
             Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 1f),
+                enabled = txtAlamat.isNotEmpty()
+            ) {
+
+            }
         }
     }
 }
