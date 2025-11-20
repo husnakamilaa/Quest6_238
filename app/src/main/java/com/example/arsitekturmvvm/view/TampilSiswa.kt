9bba2c1d27e6e2e09d216d9eeaef9956dbec16ac
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-20 16:21:42 +0700

    feat: deklarasi variable untuk menyimpan dan menampilkan data yang dikirim dari FormSiswa

--- app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
@@ -2,6 +2,9 @@ package com.example.arsitekturmvvm.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.core.util.Pair
+import com.example.arsitekturmvvm.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,5 +12,9 @@ fun TampilSiswa(
     statusUiSiswa: siswa,
     onBackButtonClicked:() ->Unit,
 ){
-
+    val items = listOf(
+        Pair(first = stringResource(R.string.nama_lengkap), second = statusUiSiswa.nama),
+        Pair(first = stringResource(R.string.gender), second = statusUiSiswa.gender),
+        Pair(first = stringResource(R.string.alamat), second = statusUiSiswa.alamat),
+    )
 }
